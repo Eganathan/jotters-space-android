4396ceac7999df818fad2b681cbf56103c513e64
Eganathan R <ammasamrith@gmail.com>
2025-02-15 20:45:23 +0530

    chore: version upgrade to the latest version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,10 +75,10 @@ dependencies {
     implementation(libs.kotlinx.coroutines.android)
 
     // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Can use kotlin Serialization as well
-    implementation("com.squareup.okhttp3:okhttp:4.10.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson) // Can use kotlin Serialization as well
+    implementation(libs.okhttp)
+    implementation(libs.logging.interceptor)
 
     //hilt dependency injection
     implementation(libs.hilt.android)
